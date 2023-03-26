3e13c337cf9a1fd70f2472c787a93e6422a02bab
Gabriel Rueda <ruedag@uwindsor.ca>
2023-03-26 11:28:52 -0400

    selectQueries.sql added

--- FinalSubmission/selectQueries.sql
+++ FinalSubmission/selectQueries.sql
@@ -1,4 +1,5 @@
 USE HOSPITAL_DB;
 
+-- Just Write all the queries we use in our program here --
 SELECT e.FirstName, e.LastName, s.SpecialName FROM Doctor as d, Employee as e, Specialization as s
 WHERE d.EmpID = e.EmpID AND d.Specialization = s.SpecialID;
