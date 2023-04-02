55465a4ce7d21961574a97f3a138124a72e4f8de
Mork <mfinerty1@hotmail.com>
2023-04-02 11:46:46 -0400

    add addNurse.py

--- FinalSubmission/addNurse.py
+++ FinalSubmission/addNurse.py
@@ -1,41 +1,8 @@
 import mysql.connector
-import datetime
-from mysql.connector.errors import InternalError
-
-# Put your functions here (make sure it's imported in main just like the way getDoctorProfile is imported)
+from actions import checkValidDate
 
 
-# Here is an example
-# It will just show the first doctor for now, but that will change
-def getDoctorProfile(myCursor):
-    myResult = doctorProfile(myCursor, 'Carlos')
-    print(f"Name: {myResult[0]}")
-    print(f"Specialization: {myResult[1]}")
-    print(f"Address: {myResult[2]}")
-    print(f"Date {myResult[3]}")
-    print(f"EmployeeID: {myResult[4]}")
-    print(f"Salary: {myResult[5]}")
-
-
-def doctorProfile(myCursor, fName, lName):
-    myCursor.execute(f"SELECT CONCAT(e.FirstName,' ', e.MiddleInitials, '. ',  e.LastName), s.SpecialName, CONCAT(e.StreetNum, ' ' , e.StreetName, ', ' , e.City, ', ', e.Province, ', ',  e.PostalCode), e.DateHired, e.SinNumber, CONCAT('$', e.Salary) FROM Doctor as d, Employee as e, Specialization as s WHERE d.EmpID = e.EmpID AND d.Specialization = s.SpecialID AND e.FirstName=\"{fName}\" AND e.LastName=\"{lName}\";")
-    myResult = myCursor.fetchall() 
-
-    return myResult[0]
-
-
-def checkValidDate(year, month, day):
-    try:
-        year = int(year)
-        month = int(month) 
-        day = int(day)
-        newDate = datetime.datetime(year,month,day)
-        return True
-    except ValueError:
-        return False
-    
-
-def addDoctor(myCursor, mydb):
+def addNurse(myCursor, mydb):
     myCursor.close()
     myCursor = mydb.cursor(buffered=True)
 
@@ -55,7 +22,6 @@ def addDoctor(myCursor, mydb):
         'Yukon': 'YT'
     }
 
-
     print("Please enter the following information: ")
     fName = input('First Name: ')
     mInitial = input('Middle Initial: ')
@@ -67,7 +33,6 @@ def addDoctor(myCursor, mydb):
     while(not checkValidDate(date[0:4], date[5:7], date[8:10])):
         print("Invalid Date!!")
         date = input('Date (YYYY-MM-DD): ')
-
 
 
     streetNum = input('Street Number: ')
@@ -89,16 +54,27 @@ def addDoctor(myCursor, mydb):
 
     myCursor.execute(f"INSERT IGNORE into Specialization VALUES((SELECT MAX(SpecialID)+1 FROM Specialization S),\"{specialization}\");")
                      
-    
     myCursor.execute(f"INSERT INTO Employee VALUES(\"{fName}\", \"{mInitial}\",\"{lName}\",{sinNumber},@NewEmpID,{salary},\"{date}\",\'{province}\',\'{city}\',{streetNum},\'{streetName}\',\'{postalCode}\');") 
                                                  
-    myCursor.execute(f"INSERT INTO Doctor VALUES(@NewEmpID, (SELECT SpecialID FROM Specialization WHERE SpecialName=\'{specialization}\'), 0);")
-    
-    # myResult = myCursor.fetchall() 
-
-    try:
-        mydb.commit()
-    except InternalError:
-        print("NOTHING")
+    myCursor.execute(f"INSERT INTO Nurse VALUES(@NewEmpID, (SELECT SpecialID FROM Specialization WHERE SpecialName=\'{specialization}\'), NULL, NULL);")
     
     myCursor.close()
+
+
+def getNurseProfile(myCursor):
+    firstName = input("First name: ")
+    lastName = input("Last name: ")
+    myResult = nurseProfile(myCursor, firstName, lastName)
+    print(f"Name: {myResult[0]}")
+    print(f"Specialization: {myResult[1]}")
+    print(f"Address: {myResult[2]}")
+    print(f"Date {myResult[3]}")
+    print(f"EmployeeID: {myResult[4]}")
+    print(f"Salary: {myResult[5]}")
+
+
+def nurseProfile(myCursor, fName, lName):
+    myCursor.execute(f"SELECT CONCAT(e.FirstName,' ', e.MiddleInitials, '. ',  e.LastName), s.SpecialName, CONCAT(e.StreetNum, ' ' , e.StreetName, ', ' , e.City, ', ', e.Province, ', ',  e.PostalCode), e.DateHired, e.SinNumber, CONCAT('$', e.Salary) FROM Nurse as n, Employee as e, Specialization as s WHERE n.EmpID = e.EmpID AND n.Specialization = s.SpecialID AND e.FirstName=\"{fName}\" AND e.LastName=\"{lName}\";")
+    myResult = myCursor.fetchall() 
+
+    return myResult[0]
